9760c6c9dae9de60501a80eb6c26d63b438281e8
Arifkhanov Azamatkhan <aaarifkhanov@edu.hse.ru>
2022-03-13 21:49:01 +0300

    improved some details in methods

--- git_repo_extract/commits_info.py
+++ git_repo_extract/commits_info.py
@@ -3,10 +3,10 @@ import json
 from dulwich.diff_tree import TreeChange
 from dulwich.repo import Repo
 import difflib
-from typing import Dict, List, Union, Optional, Any
+from typing import Dict, List, Union, Optional, Any, Tuple, Iterator
 
 
-def get_diffs_num(repos: Repo, sha1: bytes, sha2: bytes) -> List[int]:
+def get_diffs_num(repos: Repo, sha1: bytes, sha2: bytes) -> Tuple[int, int]:
     """
     A method that gives blob differences
     Args:
@@ -15,7 +15,7 @@ def get_diffs_num(repos: Repo, sha1: bytes, sha2: bytes) -> List[int]:
         :param sha2: new blob's sha
     Returns:
         :return [Added, Deleted]
-  """
+    """
 
     differences = difflib.unified_diff(repos.get_object(sha1).data.decode().splitlines(),
                                        repos.get_object(sha2).data.decode().splitlines())
@@ -26,63 +26,12 @@ def get_diffs_num(repos: Repo, sha1: bytes, sha2: bytes) -> List[int]:
             added += 1
         if line.startswith("-") and not line.startswith("--"):
             deleted += 1
-    return [added, deleted]
+    return added, deleted
 
 
-def get_commits_info(repo: Repo, limit: int = -1) -> Dict:
+def get_commits_info_floored(repo: Repo, limit: int = -1) -> Iterator[Dict[str, Any]]:
     """
-    A method returns Dictionary with info about authors commits on given repo
-
-    Dict architecture:
-    {
-      name
-      email
-      commit_id {
-        file_path {
-          blob_id
-          added
-          modified
-          deleted
-        }
-      }
-    }
-
-    Args:
-        repo: source repository
-        limit: limit of entities to check. Useful for pipeline check
-    Returns: Dict
-  """
-
-    info = {}
-    i = 0
-    for r in repo.get_walker():
-        name = r.commit.author.decode()
-        mail = name[name.find('<') + 1:-1]
-        name = name[:name.find('<')].strip()
-
-        if name not in info:
-            info[name] = {}
-            info[name]['mail'] = mail
-
-        if r.commit.id not in info[name]:
-            info[name][r.commit.id] = {}
-
-        for ch in r.changes():
-            info[name][r.commit.id][ch.new.path] = {}
-            info[name][r.commit.id][ch.new.path]['blob_id'] = ch.new.sha
-            diffs = get_diffs_num(repo, ch.old.sha, ch.new.sha)
-            info[name][r.commit.id][ch.new.path]['added'] = diffs[0]
-            info[name][r.commit.id][ch.new.path]['deleted'] = diffs[1]
-        if limit != -1 and i == limit:
-            break
-        i += 1
-    return info
-
-
-def get_commits_info_floored(repo: Repo, path: str = None, limit: int = -1) -> List[
-    Dict[str, Union[Optional[int], Any]]]:
-    """
-      A method returns Dictionary with info about authors commits on given repo
+      A method returns Dictionaries with info about authors commits on given repo
 
       Dict architecture:
       {
@@ -98,40 +47,35 @@ def get_commits_info_floored(repo: Repo, path: str = None, limit: int = -1) -> L
 
       Args:
         :param repo: source repository
-        :param path: jsonl file path, where to write
         :param limit: limit of entities to check. Useful for pipeline check
       Returns:
-        :return List if path is None (dicts will be collected in a List)
-
+        :return Iterator of dicts
     """
-
-    info = []
+    repo_url = (repo.get_config().get((b"remote", b"origin"), b"url")).decode()
     i = 0
-    for r in repo.get_walker():
-        name = r.commit.author.decode()
+    for walk in repo.get_walker():
+        name = walk.commit.author.decode()
         mail = name[name.find('<') + 1:-1]
         name = name[:name.find('<')].strip()
 
-        for ch in r.changes():
-            try:
-                content = process_change(ch, repo)
-            except:
-                continue
+        for changes in walk.changes():
+            if not isinstance(changes, list):
+                changes = [changes]
 
-            content["name"] = name
-            content["mail"] = mail
-            content["commit_id"] = r.commit.id
+            for change in changes:
+                try:
+                    content = process_change(change, repo)
+                except:
+                    continue
+                content["repo_url"] = repo_url
+                content["name"] = name
+                content["mail"] = mail
+                content["commit_id"] = walk.commit.id
 
-            if path is not None:
-                write_down_content(content, path)
-            else:
-                info.append(content)
-
-        if limit != -1 and i == limit:
-            break
-        i += 1
-
-    return info
+                if limit != -1 and i == limit:
+                    break
+                i += 1
+                yield content
 
 
 def process_change(ch: TreeChange, repo: Repo) -> Dict:
@@ -144,21 +88,15 @@ def process_change(ch: TreeChange, repo: Repo) -> Dict:
     Returns
         :return: dictionary with necessary elements
     """
-    content = {}
+    content = {"path": ch.new.path, "blob_id": ch.new.sha}
 
     if ch.old.sha is None:
-        content["path"] = ch.new.path
-        content["blob_id"] = ch.new.sha
         content["added"] = len(repo.get_object(ch.new.sha).data.decode().splitlines())
         content["deleted"] = 0
     elif ch.new.sha is None:
-        content["path"] = ch.old.path
-        content["blob_id"] = ch.old.sha
         content["deleted"] = len(repo.get_object(ch.old.sha).data.decode().splitlines())
         content["added"] = 0
     else:
-        content["path"] = ch.new.path
-        content["blob_id"] = ch.new.sha
         diffs = get_diffs_num(repo, ch.old.sha, ch.new.sha)
         content["added"] = diffs[0]
         content["deleted"] = diffs[1]
--- git_repo_extract/star_track.py
+++ git_repo_extract/star_track.py
@@ -8,7 +8,7 @@ from github import Github, RateLimitExceededException
 from typing import Dict, Any, List
 
 
-def get_stargazer_info(repo_name: str, github_key: str, path: str = None,
+def get_stargazer_info(repo_name: str, github_key: str,
                        max_user_stars: int = 300,
                        per_page: int = 80) -> Any[List[Dict], Dict]:
     """
@@ -17,7 +17,6 @@ def get_stargazer_info(repo_name: str, github_key: str, path: str = None,
 
     :param repo_name: name of repository which stargazers to investigate
     :param github_key: personal github key to get access to the github API
-    :param path: path to .jsonl file to log all the repositories
     :param max_user_stars: skip users with bigger amount of stars
     :param per_page: pagination parameter for GitHub request API
 
@@ -26,45 +25,23 @@ def get_stargazer_info(repo_name: str, github_key: str, path: str = None,
     top_repos = Counter()
     account = Github(github_key, per_page=per_page)
 
-    repo = None
-    while True:
-        try:
-            repo = account.get_repo(repo_name)
-            break
-        except RateLimitExceededException:
-            sleep_until_unblock(account)
-            continue
-        # other errors should strike
+    sleep_until_unblock(account)
+    repo = account.get_repo(repo_name)
 
     users = []
     for user in tqdm(repo.get_stargazers()):
-        try:
-            user_stars = user.get_starred().totalCount
-            if user_stars > max_user_stars:
-                continue
-            users.append(user)
+        sleep_until_unblock(account)  # not sure this is optimal way. It will be called each iteration
+        user_stars = user.get_starred().totalCount
+        if user_stars > max_user_stars:
             continue
-        except RateLimitExceededException:
-            sleep_until_unblock(account)
-            user_stars = user.get_starred().totalCount
-            if user_stars > max_user_stars:
-                continue
-            users.append(user)
-            continue
+        users.append(user)
 
     for user in tqdm(users):
-        try:
-            reps = user.get_starred()
-            for r in reps:
-                top_repos[r.full_name] += 1
-        except RateLimitExceededException:
-            sleep_until_unblock(account)
-            reps = user.get_starred()
-            for r in reps:
-                top_repos[r.full_name] += 1
+        sleep_until_unblock(account)
+        reps = user.get_starred()
+        for r in reps:
+            top_repos[r.full_name] += 1
 
-    if path is not None:
-        write_down_content(dict(top_repos), path)
     return top_repos
 
 
@@ -79,5 +56,5 @@ def sleep_until_unblock(account: Github) -> None:
     print('search remaining: {}'.format(search_rate_limit.remaining))
     reset_timestamp = calendar.timegm(search_rate_limit.reset.timetuple())
     # add 10 seconds to be sure the rate limit has been reset
-    sleep_time = reset_timestamp - calendar.timegm(time.gmtime()) + 10
+    sleep_time = max(0.01, reset_timestamp - calendar.timegm(time.gmtime()) + 10)
     time.sleep(sleep_time)
